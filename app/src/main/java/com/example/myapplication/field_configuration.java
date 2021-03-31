5e670b729dac0c622b0efc66aede78f535f44a19
abhishekshah1998 <abhishekshah1998@gmail.com>
2021-03-31 16:04:34 +0530

    screen 1

--- app/src/main/java/com/example/myapplication/field_configuration.java
+++ app/src/main/java/com/example/myapplication/field_configuration.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GsmAuthenticationActivity extends AppCompatActivity {
+public class field_configuration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gsm_authentication);
+        setContentView(R.layout.field_configuration);
     }
 }
