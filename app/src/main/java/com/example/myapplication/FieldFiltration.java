ba2f600d06a2214d59ff73ba4d269097bfe5efe8
abhishekshah1998 <abhishekshah1998@gmail.com>
2021-04-02 20:16:59 +0530

    screen 1

---
+++ app/src/main/java/com/example/myapplication/FieldFiltration.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FieldFiltration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.field_filtration);
+    }
+}
