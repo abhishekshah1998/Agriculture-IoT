efc9b8262ef17363038bd9427a774fe49d71525b
abhishekshah1998 <abhishekshah1998@gmail.com>
2021-04-20 23:16:27 +0530

    screen 1

--- app/src/main/java/com/example/myapplication/settings.java
+++ app/src/main/java/com/example/myapplication/settings.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ReportSelection extends AppCompatActivity {
+public class settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.report_selection);
+        setContentView(R.layout.settings);
     }
 }
