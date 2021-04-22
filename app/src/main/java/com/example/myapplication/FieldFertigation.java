4bdbec43a05a4c643fa0f070c06e984884c23e63
vk2607 <vinodkamat98@gmail.com>
2021-04-22 23:57:19 +0530

    Field Fertigation Dropdown

--- app/src/main/java/com/example/myapplication/FieldFertigation.java
+++ app/src/main/java/com/example/myapplication/FieldFertigation.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.app.DatePickerDialog;
@@ -18,20 +19,35 @@ import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 
 import java.util.Calendar;
+import java.util.List;
 
 public class FieldFertigation extends AppCompatActivity {
-
+    Spinner triggerFromSpinner, fieldNumberSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.field_fertigation);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        String[] filedNumberArray = new String[]{};
+        List<String> fieldNumberList = new ArrayList<String>(Arrays.asList(filedNumberArray));
+        for (int i = 1; i<=12;i++){
+            fieldNumberList.add(Integer.toString(i));
+        }
+        filedNumberArray = fieldNumberList.toArray(filedNumberArray);
+        fieldNumberSpinner = (Spinner)findViewById(R.id.colorSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.spinner_list, filedNumberArray);
+        adapter.setDropDownViewResource(R.layout.spinner_list);
+        fieldNumberSpinner.setAdapter(adapter);
 
     }
     @Override
